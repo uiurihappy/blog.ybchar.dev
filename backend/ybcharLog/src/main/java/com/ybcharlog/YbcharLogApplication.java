f9563d1c976b38b8cfc8505ad32b551b0b912d75
ybchar <uiurihappy@sju.ac.kr>
2023-03-31 17:50:12 +0900

    First Commit

---
+++ backend/ybcharLog/src/main/java/com/ybcharlog/YbcharLogApplication.java
@@ -0,0 +1,13 @@
+package com.ybcharlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbcharLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YbcharLogApplication.class, args);
+    }
+
+}
---
+++ backend/ybcharLog/src/test/java/com/ybcharlog/YbcharLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybcharlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbcharLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

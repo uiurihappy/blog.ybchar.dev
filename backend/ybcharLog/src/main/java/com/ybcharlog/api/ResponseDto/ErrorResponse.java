b29459b689f09a39950fc76f15441dea92351b9b
ybchar <uiurihappy@sju.ac.kr>
2023-04-03 17:08:07 +0900

    feat: ExceptionController addExpect

--- backend/ybcharLog/src/main/java/com/ybcharlog/api/ResponseDto/ErrorResponse.java
+++ backend/ybcharLog/src/main/java/com/ybcharlog/api/ResponseDto/ErrorResponse.java
@@ -23,9 +23,25 @@ public class ErrorResponse {
 	private final String code;
 	private final String message;
 
-	private Map<String, String> validation = new HashMap<>();
+	/* Map 사용 지양하는 이유
+		- 고정타입 문제
+		- 캐스팅 문제
+		- 컴파일 에러가 발생 안하는 문제
+		- 사용처가 불분명해지는 문제ㄹ
+		- 응답 필드에 뭐가 들어가는지 알 수 없는 문제
+		- 불변성문제
+	 */
+	private final Map<String, String> validation = new HashMap<>();
 
 	public void addValidation(String fieldName, String errorMessage) {
+//		ValidationTuple tuple = new ValidationTuple(fieldName, errorMessage);
 		this.validation.put(fieldName, errorMessage);
+	}
+
+	// 이렇게 말고 dto를 선언해서 사용해서 하는 방식이 제일 깔금하다 @Data를 사용한다든지..
+	@RequiredArgsConstructor
+	private class ValidationTuple {
+		private final String fieldName;
+		private final String errroMessage;
 	}
 }
--- backend/ybcharLog/src/test/java/com/ybcharlog/api/controller/PostControllerTest.java
+++ backend/ybcharLog/src/test/java/com/ybcharlog/api/controller/PostControllerTest.java
@@ -53,6 +53,7 @@ class PostControllerTest {
 //                .andExpect(content().string("{}"))
                 .andExpect(jsonPath("$.code").value("400"))
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("title input please"))
                 .andDo(print());
 
     }

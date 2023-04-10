168512b43eef4fed0bbd6ca4c71dea8f9d815067
ybchar <uiurihappy@sju.ac.kr>
2023-04-11 05:16:56 +0900

    feat: Generated

---
+++ backend/ybcharLog/src/main/generated/com/ybcharlog/api/domain/QPost.java
@@ -0,0 +1,62 @@
+package com.ybcharlog.api.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = 1778936000L;
+
+    public static final QPost post = new QPost("post");
+
+    public final com.ybcharlog.api.Common.QBaseEntity _super = new com.ybcharlog.api.Common.QBaseEntity(this);
+
+    public final ListPath<Comment, QComment> comments = this.<Comment, QComment>createList("comments", Comment.class, QComment.class, PathInits.DIRECT2);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final StringPath lastModifiedBy = _super.lastModifiedBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedDate = _super.lastModifiedDate;
+
+    public final NumberPath<Integer> likeCount = createNumber("likeCount", Integer.class);
+
+    public final StringPath title = createString("title");
+
+    public final NumberPath<Integer> viewCount = createNumber("viewCount", Integer.class);
+
+    public QPost(String variable) {
+        super(Post.class, forVariable(variable));
+    }
+
+    public QPost(Path<? extends Post> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPost(PathMetadata metadata) {
+        super(Post.class, metadata);
+    }
+
+}
+

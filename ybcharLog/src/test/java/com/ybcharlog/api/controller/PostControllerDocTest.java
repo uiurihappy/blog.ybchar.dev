cb1fa6e4189e0646f1551a5dac43a7239cb59662
ybchar <uiurihappy@sju.ac.kr>
2023-04-21 11:18:40 +0900

    feat: Post docs Test

--- ybcharLog/src/test/java/com/ybcharlog/api/controller/PostControllerDocTest.java
+++ ybcharLog/src/test/java/com/ybcharlog/api/controller/PostControllerDocTest.java
@@ -2,7 +2,11 @@ package com.ybcharlog.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ybcharlog.api.RequestDto.post.PostCreateDto;
+import com.ybcharlog.api.ResponseDto.comment.CommentResponse;
+import com.ybcharlog.api.domain.comment.Comment;
 import com.ybcharlog.api.domain.post.Post;
+import com.ybcharlog.api.mapper.comment.GetCommentResDtoMapper;
+import com.ybcharlog.api.repository.comment.CommentRepository;
 import com.ybcharlog.api.repository.post.PostRepository;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
@@ -62,6 +66,8 @@ public class PostControllerDocTest {
 
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
 //    @BeforeEach
 //    public void setUp(WebApplicationContext webApplicationContext,
@@ -90,6 +96,8 @@ public class PostControllerDocTest {
                 .isDeleted(0)
                 .build();
         postRepository.save(post);
+//        CommentResponse postComment = GetCommentResDtoMapper.INSTANCE.toDto(commentRepository.save(Comment.initComment("나 유저 1", null
+//                , "유저 1의 댓글", 0, 1, 0, post)));
 
         // expected
         this.mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", post.getId())
@@ -111,18 +119,18 @@ public class PostControllerDocTest {
                                 fieldWithPath("display").description("게시글 viewStatus"),
                                 fieldWithPath("isDeleted").description("게시글 삭제 상태"),
 
-//                                fieldWithPath("comments").type(JsonFieldType.ARRAY).description("댓글"),
-                                fieldWithPath("comments.[].id").description("댓글 ID"),
-                                fieldWithPath("comments.[].username").description("댓글 사용자 이름"),
-                                fieldWithPath("comments.[].password").description("댓글 비밀번호").optional(),
-                                fieldWithPath("comments.[].commentContent").description("댓글 내용"),
-                                fieldWithPath("comments.[].secretStatus").description("댓글 비밀번호 상태"),
-                                fieldWithPath("comments.[].display").description("게시글 viewStatus"),
-                                fieldWithPath("comments.[].isDeleted").description("게시글 삭제 상태"),
-                                fieldWithPath("comments.[].createdAt").description("생성 일자"),
-                                fieldWithPath("comments.[].lastModifiedDate").description("수정 일자"),
-                                fieldWithPath("comments.[].createdBy").description("생성한 유저 ID"),
-                                fieldWithPath("comments.[].lastModifiedBy").description("수정한 유저 ID"),
+                                fieldWithPath("comments.[]").type(JsonFieldType.ARRAY).description("댓글"),
+//                                fieldWithPath("comments.[].id").description("댓글 ID"),
+//                                fieldWithPath("comments.[].username").description("댓글 사용자 이름"),
+//                                fieldWithPath("comments.[].password").description("댓글 비밀번호").optional(),
+//                                fieldWithPath("comments.[].commentContent").description("댓글 내용"),
+//                                fieldWithPath("comments.[].secretStatus").description("댓글 비밀번호 상태"),
+//                                fieldWithPath("comments.[].display").description("게시글 viewStatus"),
+//                                fieldWithPath("comments.[].isDeleted").description("게시글 삭제 상태"),
+//                                fieldWithPath("comments.[].createdAt").description("생성 일자"),
+//                                fieldWithPath("comments.[].lastModifiedDate").description("수정 일자"),
+//                                fieldWithPath("comments.[].createdBy").description("생성한 유저 ID"),
+//                                fieldWithPath("comments.[].lastModifiedBy").description("수정한 유저 ID"),
                                 fieldWithPath("createdAt").description("생성 일자"),
                                 fieldWithPath("lastModifiedDate").description("수정 일자"),
                                 fieldWithPath("createdBy").description("생성한 유저 ID"),
@@ -176,26 +184,28 @@ public class PostControllerDocTest {
                 .content("글 내용입니다.")
                 .viewCount(0)
                 .likeCount(0)
-                .display(0)
+                .display(1)
                 .isDeleted(0)
                 .build();
         postRepository.save(post);
 
-        String json = objectMapper.writeValueAsString(post);     // Javascript의 JSON.stringfy(object) 느낌
-        System.out.println(json);
-        Optional<Post> findPost = postRepository.findById(1L);
+//        String json = objectMapper.writeValueAsString(post);     // Javascript의 JSON.stringfy(object) 느낌
+//        System.out.println(json);
+//        Optional<Post> findPost = postRepository.findById(1L);
 
         // expected
-        this.mockMvc.perform(delete("/posts/delete/{postId}", post.getId())
-                        .accept(APPLICATION_JSON)
-                        .contentType(APPLICATION_JSON)
-                        .content(json))
+        this.mockMvc.perform(RestDocumentationRequestBuilders.delete("/posts/delete/{postId}", post.getId())
+                        .accept(APPLICATION_JSON))
+//                        .contentType(APPLICATION_JSON)
+//                        .content(json))
                 .andExpect(status().isOk())
                 .andDo(document("deletePost",
-                        requestFields(
-                                fieldWithPath("postId").description("게시글 Id")
-                        ),
-                        relaxedRequestFields()
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 ID")
+                        )
+//                        relaxedRequestFields()
                 ));
     }
 }

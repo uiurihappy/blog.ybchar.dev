09e12819434f38de5aa57115d4c01936add3d1c7
ybchar <uiurihappy@sju.ac.kr>
2023-04-21 17:43:12 +0900

    feat: Header vue

--- ybcharLog/front-end/src/components/Header.vue
+++ ybcharLog/front-end/src/components/Header.vue
@@ -1,19 +1,65 @@
 <template>
-  <el-header class="header">
-    <!--    <RouterLink to="/">Home</RouterLink>-->
-    <!--    <RouterLink to="/write">글 작성</RouterLink>-->
-    <el-menu mode="horizontal" router>
-      <el-menu-item index="/">Home</el-menu-item>
-      <el-menu-item index="/write">글 작성</el-menu-item>
-    </el-menu>
-  </el-header>
+  <div class="blog">
+    <el-header class="header">
+      <div class="logo">
+        <el-menu-item class="indexHeader" index="/">ybchar blog</el-menu-item>
+      </div>
+      <div class="menu">
+        <el-menu mode="horizontal" router>
+          <el-menu-item class="saveHeader" index="/write">글 작성</el-menu-item>
+        </el-menu>
+      </div>
+    </el-header>
+    <el-main class="main">
+      <router-view></router-view>
+    </el-main>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<style lang="scss" scoped>
+@font-face {
+  font-family: 'jua';
+  src: url('../../public/fonts/BMJUA_ttf.ttf');
+}
+@font-face {
+  font-family: 'dohyeon';
+  src: url('../../public/fonts/BMDOHYEON_ttf.ttf');
+}
+.blog {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
 
-<style scoped>
 .header {
-  padding: 0;
-  height: 60px;
+  height: auto;
+  background-color: #fff;
+  border-bottom: 1px solid #e6e6e6;
+}
+
+.logo {
+  font-family: 'jua';
+  font-size: 24px;
+  color: black;
+  padding: 10px;
+}
+
+.indexHeader {
+  color: black;
+}
+
+.menu {
+  padding: 10px;
+}
+
+.el-menu {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.main {
+  flex-grow: 1;
+  margin: 20px 0;
 }
 </style>

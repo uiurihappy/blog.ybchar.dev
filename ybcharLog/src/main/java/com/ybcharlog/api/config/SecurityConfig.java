2b94e040d255bd1d8b3a36b1fbf9018f61b79807
ybchar <uiurihappy@sju.ac.kr>
2023-05-07 15:08:23 +0900

    feat: security custom config

--- ybcharLog/src/main/java/com/ybcharlog/api/config/SecurityConfig.java
+++ ybcharLog/src/main/java/com/ybcharlog/api/config/SecurityConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.util.AntPathMatcher;
@@ -31,7 +37,31 @@ public class SecurityConfig {
                 .requestMatchers("/posts/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin()
+                    .loginPage("/auth/login")
+                    .loginProcessingUrl("/auth/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/posts/list?page=1&size=12")
+                .and()
+                .userDetailsService(userDetailsService())
+                    .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails user = User
+                .withUsername("ybchar")
+                .password("qwer1234")
+                .roles("ADMIN").build();
+        manager.createUser(user);
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }

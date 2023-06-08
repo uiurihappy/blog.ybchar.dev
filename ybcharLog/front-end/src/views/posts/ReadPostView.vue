188568b18879eda51439334e7ee94bf7f9d87920
ybchar <uiurihappy@sju.ac.kr>
2023-06-08 14:28:57 +0900

    feat: toast-ui viewer ReadView

--- ybcharLog/front-end/src/views/posts/ReadPostView.vue
+++ ybcharLog/front-end/src/views/posts/ReadPostView.vue
@@ -24,13 +24,8 @@
 
     <el-row>
       <el-col>
-        <div class="content">
-          <div v-if="isCodeBlock">
-            <pre>
-              <code class="code-block">{{ codeBlockContent }}</code>
-            </pre>
-          </div>
-          <div v-html="renderedContent"></div>
+        <div>
+          <div id="viewer" ref="viewer" class="row"></div>
         </div>
       </el-col>
     </el-row>
@@ -99,18 +94,24 @@
 </template>
 
 <script lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import axios from 'axios';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
-import MarkdownIt from 'markdown-it';
-
+import Editor from '@toast-ui/editor';
+import jwt_decode from 'jwt-decode';
+// Import prismjs
+import Prism from 'prismjs';
 import type { Comments } from '../../common/comments/comments.interface';
 import type { Posts } from '../../common/posts/posts.interface';
 import { getFormattedDate } from '../../common/tools/dateFormat.tool';
-import jwt_decode from 'jwt-decode';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import type Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 dayjs.extend(timezone);
 
 interface AccessToken {
@@ -125,13 +126,11 @@ export default {
       required: true,
     },
   },
-
   setup(props) {
     const username = ref('');
     const password = ref('');
     const secretStatus = ref(0);
     const commentContent = ref('');
-    const renderedContent = ref('');
 
     let userRole = ref('');
     const accessToken: string | null = sessionStorage.getItem('accessToken');
@@ -155,9 +154,6 @@ export default {
         return false;
       }
     };
-
-    let isCodeBlock = ref(false);
-    let codeBlockContent = ref('');
 
     const post = ref<Posts>({
       id: 0,
@@ -196,35 +192,53 @@ export default {
           alert('댓글 작성이 실패되었습니다.');
         });
     };
-
-    onMounted(() => {
-      const md = new MarkdownIt();
+    const fetchData = async () => {
       axios
         .get(`/api/posts/${props.postId}`)
         .then(result => {
           post.value = result.data;
-          renderedContent.value = md.render(result.data.content);
         })
         .catch(() => {
           alert('글 조회에 실패하였습니다.');
         });
-    });
+    };
 
+    const viewerRef = ref<Viewer>(null);
+
+    onMounted(async () => {
+      const viewer = Editor.factory({
+        el: document.querySelector('#viewer'),
+        viewer: true,
+        // Initialize with an empty string
+        initialValue: '',
+        plugins: [[codeSyntaxHighlight, { highlighter: Prism }]],
+      });
+      viewerRef.value = viewer;
+
+      // Watch for changes in post.value.content
+      watch(
+        () => post.value.content,
+        newContent => {
+          viewerRef.value!.setMarkdown(newContent);
+        }
+      );
+
+      // Fetch data after setting up the watcher
+      await fetchData();
+    });
     return {
       username,
       password,
       secretStatus,
       commentContent,
-      renderedContent,
       userRole,
-      isCodeBlock,
-      codeBlockContent,
       post,
       moveToEdit,
       writeComment,
       isAccessTokenValid,
       getFormattedDate,
       dayjs,
+      viewerRef,
     };
   },
 };

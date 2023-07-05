96cf254f7c3ac757a434aaf6b10c91f38275e2bd
ybchar <uiurihappy@sju.ac.kr>
2023-07-05 23:31:23 +0900

    fix: Comment cascade

--- ybcharLog/src/main/java/com/ybcharlog/api/domain/comment/Comment.java
+++ ybcharLog/src/main/java/com/ybcharlog/api/domain/comment/Comment.java
@@ -39,7 +39,7 @@ public class Comment extends BaseEntity {
     @Column(columnDefinition = "tinyint(3) not null default 0 COMMENT '비밀 댓글 상태'")
     private Integer secretStatus;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "postId")
     @JsonIgnore
     private Post post;

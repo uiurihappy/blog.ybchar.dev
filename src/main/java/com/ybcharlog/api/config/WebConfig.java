567f1bda8432ae9abc4b892b978140482f49f495
ybchar <uiurihappy@sju.ac.kr>
2023-10-05 11:00:18 +0900

    fix: cors Mapping

--- src/main/java/com/ybcharlog/api/config/WebConfig.java
+++ src/main/java/com/ybcharlog/api/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.ybcharlog.api.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -27,12 +30,15 @@ public class WebConfig implements WebMvcConfigurer {
 	private String apiUrl;
 
 	@Override
-	public void addCorsMappings(CorsRegistry corsRegistry) {
-		corsRegistry.addMapping("/**")
-				.allowedOrigins("http://localhost:" + portNumber, "http://localhost:8081",
-						"http://localhost:8080", "http://localhost:9000", s3Url,
-						prodUrl, apiUrl,
-						subProdUrl, subApiUrl)
-				.maxAge(3600L);
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods(Arrays.stream(HttpMethod.values())
+						.map(HttpMethod::name)
+						.toArray(String[]::new))
+				.allowedHeaders("Authorization", "Content-Type")
+				.exposedHeaders("Custom-Header")
+				.allowCredentials(true)
+				.allowedHeaders("*");
 	}
 }

ba6db8630f89a89810ee9c127e7a0bd44cc16aa2
ybchar <uiurihappy@sju.ac.kr>
2023-10-06 14:14:18 +0900

    fix: AWS S3 Config

--- src/main/java/com/ybcharlog/api/config/S3Config.java
+++ src/main/java/com/ybcharlog/api/config/S3Config.java
@@ -1,10 +1,10 @@
 package com.ybcharlog.api.config;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,10 +22,14 @@ public class S3Config {
 	private String region;
 
 	@Bean
-	public AmazonS3Client amazonS3Client() {
+	public AmazonS3 amazonS3Client() {
 		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-		AmazonS3Client amazonS3Client = new AmazonS3Client(credentials);
-		amazonS3Client.setRegion(Region.getRegion(Regions.fromName(region)));
-		return amazonS3Client;
+
+		return AmazonS3ClientBuilder
+				.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(region)
+				.build();
 	}
+
 }

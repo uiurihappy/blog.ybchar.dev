1bd6666a2d0198fbc090bcec7e3e8d4a55b381c0
ybchar <uiurihappy@sju.ac.kr>
2023-10-06 16:48:18 +0900

    fix: AWS S3 Image Convert

--- src/main/java/com/ybcharlog/api/service/AWS/S3UploaderService.java
+++ src/main/java/com/ybcharlog/api/service/AWS/S3UploaderService.java
@@ -14,7 +14,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor    // final 멤버변수가 있으면 생성자 항목에 포함시킴
@@ -34,11 +36,11 @@ public class S3UploaderService {
 	public String upload(MultipartFile multipartFile, String dirName) throws IOException {
 		File uploadFile = convert(multipartFile)
 				.orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-		return upload(uploadFile, dirName);
+		return upload(uploadFile, dirName, multipartFile.getOriginalFilename());
 	}
 
-	private String upload(File uploadFile, String dirName) {
-		String fileName = env + "/" + dirName + "/" + uploadFile.getName();
+	private String upload(File uploadFile, String dirName, String originName) {
+		String fileName = env + "/" + dirName + "/" + UUID.randomUUID() + originName;
 		String uploadImageUrl = putS3(uploadFile, fileName);
 
 		removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
@@ -72,8 +74,8 @@ public class S3UploaderService {
 		}
 	}
 
-	private Optional<File> convert(MultipartFile file) throws  IOException {
-		File convertFile = new File(file.getOriginalFilename());
+	private Optional<File> convert(MultipartFile file) throws IOException {
+		File convertFile = new File(Objects.requireNonNull(file.getOriginalFilename()) + "/" + UUID.randomUUID());
 		if(convertFile.createNewFile()) {
 			try (FileOutputStream fos = new FileOutputStream(convertFile)) {
 				fos.write(file.getBytes());
